3d3a2b667ccf484060fe77a68d38be3e0897d768
nikiasek <nikiasek50@gmail.com>
2024-02-23 10:22:08 +0100

    asdsa

--- deleni.py
+++ deleni.py
@@ -1,6 +1,15 @@
 a, b = int(input("Zadejte hodnotu A\n")), int(input("Zadejte hodnotu B\n"))
 
-if a != 0 or b !=0:
+if (a !=0) and (b !=0):
     if a > b:
         if (a%b) == 0:
             print(f"...\n{a//b}")
+        else:
+            print("...\nNEJDE DĚLIT")
+    else:
+        if(b%a) == 0:
+            print(f"...\n{b//a}")
+        else:
+            print("...\nNEJDE DĚLIT")
+else:
+    print("...\nNULOU NEMŮŽEME DĚLIT")
